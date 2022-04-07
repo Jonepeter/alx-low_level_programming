6bd4da97c42d14aea59d384fcab6e8b58ff2419b
Petros <petros@mtu.edu.et>
2022-04-07 18:05:36 +0300

    commit printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,9 @@
 #include<stdio.h>
-/*
- * main: entry point
- * return: printing texts and ends with new line using printf 
-*/
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
