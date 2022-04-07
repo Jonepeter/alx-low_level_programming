059e23fb307c3022c7a5d24007e205894c093826
Petros <petros@mtu.edu.et>
2022-04-07 18:12:43 +0300

    commit size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,17 +1,15 @@
-#include <stdio.h>
+#include<stdio.h>
 int main(void)
 {
-  int a;
-  long int b;
-  long long int c;
-  char d;
-  float f;
-  
-
-  printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(d));
-  printf("Size of a short: %lu byte(s)\n", (short)sizeof(p));
-  printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(a));
-  printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(b));
-  printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(c));
-  return (0);
+int a;
+long int b;
+long long int c;
+char d;
+float f;
+printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(d));
+printf("Size of a short: %lu byte(s)\n", (short)sizeof(p));
+printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(a));
+printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(b));
+printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(c));
+return (0);
 }
