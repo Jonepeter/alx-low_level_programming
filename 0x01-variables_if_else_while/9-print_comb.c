58749f02a6d494c2268c6144cf27903c325c8d69
Petros <petros@mtu.edu.et>
2022-04-08 18:11:22 +0300

    commit combination

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+/**
+ * main - Print combination of single digit 
+ *
+ * Return:  Always 0
+ */
+int main(void)
+{
+int i;
+for (i = 0; i < 10; i++)
+{
+putchar(i%10 + '0');
+if(i==9){continue;}
+putchar(',');
+putchar(' ');
+}
+return (0);
+}
