80f89b8e2305b8895ebeba7bd4ed958207c19e76
Petros <petros@mtu.edu.et>
2022-04-08 21:09:55 +0300

    commit print comb3

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,26 +8,28 @@
  */
 int main(void)
 {
-  int num,i;
-  for (num = 0; num < 10; num++)
-    {
-      for(i = 0; i < 10; i++)
-	{
-	  if (num > i || num == i)
-	    {
-	      continue;
-	    }
-	  else
-	    {
-	      putchar(num + '0');
-	      putchar(i + '0');
-	      if (num == 8 && i==9)
-		continue;
-	      putchar(',');
-	      putchar(' ');
-	    }
-	}
-    }
-  putchar('\n');
-  return (0);
+int num,i;
+for (num = 0; num < 10; num++)
+{
+for(i = 0; i < 10; i++)
+{
+if (num > i || num == i)
+{
+continue;
+}
+else
+{
+putchar(num + '0');
+putchar(i + '0');
+if (num == 8 && i==9)
+{
+continue;
+}
+putchar(',');
+putchar(' ');
+}
+}
+}
+putchar('\n');
+return (0);
 }
