edbc9a72db675d92621279b37cdb4e9ebd1b2bf3
Petros <petros@mtu.edu.et>
2022-04-08 16:56:54 +0300

    print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+ * main - Prints the alphabet in lowercase.
+ *
+ * Return: Always 0.
+ */
+int main()
+{
+char al = 'a';
+while(al<='z')
+{
+putchar(al);
+al++;
+}
+putchar("\n");
+return (0);
+}
