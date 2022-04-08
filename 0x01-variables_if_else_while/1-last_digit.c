2504ea0ebd728cf067632584a649e14082f11d1e
Petros <petros@mtu.edu.et>
2022-04-08 16:06:00 +0300

    commit last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -5,8 +5,9 @@
 
 /* betty style doc for function main goes there */
 /**
- *main: entry point  
- *return: Always 0 (success)   
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
