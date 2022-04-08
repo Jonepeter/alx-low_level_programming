434cb9229a9b1c3c449f68b2a298157ddd74b16a
Petros <petros@mtu.edu.et>
2022-04-08 17:23:48 +0300

    commit 4 print alphabet

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+/**
+ * main - print the alphabet except q and e
+ * return - Always 0
+ */
+int main(void)
+{
+char l;
+for (l='a'; l<='z';l++)
+{
+if (l != 'e' && l !='q')
+{
+putchar(l);
+}
+}
+putchar('\n');
+return (0);
+}
