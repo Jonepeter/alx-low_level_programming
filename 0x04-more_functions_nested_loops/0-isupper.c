2bcf498718822291bfe6a7bd3a6ad733fdddffa7
Petros <petros@mtu.edu.et>
2022-04-14 16:02:26 +0300

    commit is upper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,18 +1,14 @@
 #include "main.h"
-
 /**
- * main - the program that checks upper case letter 
+ * _isupper - Checks for uppercase characters.
+ * @c: The character to be checked.
  *
- * Return: Always  
+ * Return: 1 if character is uppercase, 0 otherwise.
  */
 int _isupper(int c)
 {
 	if (c >= 'A' && c <= 'Z')
-	{
 		return (1);
-	}
 	else
-	{
 		return (0);
-	}
 }
