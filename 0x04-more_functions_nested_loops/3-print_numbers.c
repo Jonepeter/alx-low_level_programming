3d3a9e78dbcd86620e65fb876ca156e2af355960
Petros <petros@mtu.edu.et>
2022-04-14 16:25:43 +0300

    print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * mul - Multiples two integers.
+ * @a: The first integer to be multiplied.
+ * @b: The second integer to be multiplied.
+ *
+ * Return: The result of the multiplication.
+ */
+void print_numbers(void)
+{
+	int i;
+	for (i = 0; i < 10; i++)
+	{
+		_putchar(i + '0');
+	}
+	_putchar('\n');
+}
