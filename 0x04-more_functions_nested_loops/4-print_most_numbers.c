072946f7699b50aa02c0f26f914db3e5d1d4a334
Petros <petros@mtu.edu.et>
2022-04-14 16:48:06 +0300

    print most number

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -3,14 +3,15 @@
 /**
  * print_most_number - the number from 0 - 9 except 2 and 4 
  * 
+ * Return: void
  */
 void print_most_numbers(void)
 {
-	int i;
+	char i;
 
-	for (i = 0; i <= 9; i++)
+	for (i = '0'; i <= '9'; i++)
 	{
-		if (i != 2 && i != 4)
+		if (i != '2' && i != '4')
 			_putchar(i + '0')		
 	}
 	_putchar('\n');
