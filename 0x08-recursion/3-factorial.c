c9679851881511f9d8c18d47cd019b85ec0c92cf
Petros <petros@mtu.edu.et>
2022-04-27 12:31:46 +0300

    factorial

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _strlen_recursion - Returns Length of String
+ * @s: string
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int factorial(int n)
+{
+	if(n>0)
+	{
+		return(n*factorial(n-1))
+	}
+	return (0);
+}
