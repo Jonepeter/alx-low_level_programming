c9ae906d89b71ca9095f14431bc075a1b3a6f5d8
Petros <petros@mtu.edu.et>
2022-05-19 16:39:05 +0300

    print list

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,8 +1,7 @@
 #ifndef LISTS_H
 #define LISTS_H
+
 #include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
 
 /**
  * struct list_s - singly linked list
@@ -11,6 +10,7 @@
  * @next: points to the next node
  *
  * Description: singly linked list node structure
+ *              for project
  */
 typedef struct list_s
 {
@@ -24,4 +24,5 @@ size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
-#endif
+
+#endif /* LISTS_H */
